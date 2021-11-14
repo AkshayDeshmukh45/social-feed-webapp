40768672c271524c29d851c96bc38f6cbb60a270
Akshay Deshmukh <deshmukhaksh@outlook.com>
2021-11-15 00:22:21 +0530

    THIRD COMMIT

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Header.css";
-import { Button, Input,Avatar} from "antd";
+import { Button, Input, Avatar, Layout } from "antd";
 // import logo from './logo.png';
 // import {Link} from 'react-router-dom';
 import {
@@ -11,47 +11,32 @@ import {
 } from "@ant-design/icons";
 
 function Header() {
+  const { Header } = Layout;
   const { Search } = Input;
   const onSearch = (value) => console.log(value);
 
   return (
     <div className="header">
-      <div className="home-tab">
-        <section>
-          <div>
-            <HomeOutlined />
-            Home
-          </div>
-          <div className="notification-tab">
-            <NotificationOutlined />
-            Norifications
-          </div>
-          <div className="messages-tab">
-            <MessageOutlined />
-            Messages
-          </div>
-
-          <div className="profile">
-            {/* <Link to="/">
-        <img src="https://www.freepik.com/free-vector/destination-concept-international-travel-journey-red-pointer-with-grey-world-map-inside_10817176.htm#page=1&query=location%20logo&from_query=location%20logo%20png&position=2&from_view=search" alt="App logo">Trip Gyan</img>
-      </Link>   */}
-          </div>
-          <div className="Input-search">
-            <Search
-              placeholder="Search Tripgyan"
-              allowClear
-              onSearch={Search}
-              style={{ width: 270 }}
-            />
-          </div>
-          <div className="Avatar">
+      <Layout style={{ background: "white" }}>
+        <Header className="header" style={{ background: "white" }}>
+          <HomeOutlined />
+          Home
+          <NotificationOutlined />
+          Notifications
+          <MessageOutlined />
+          Messages
+          <Search
+            placeholder="Search Tripgyan"
+            allowClear
+            onSearch={Search}
+            style={{ width: "15vw" }}
+          />
           <Avatar icon={<UserOutlined />} />
-          </div>
-          <div>
-            <Button type="primary" style={{background:"red"}}>Add Post</Button>
-          </div>
-        </section>
-      </div>
+          <Button type="primary" style={{ background: "red" }}>
+            Add Post
+          </Button>
+        </Header>
+      </Layout>
     </div>
   );
 }
