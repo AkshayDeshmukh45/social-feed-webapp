bf222a2626fb5f1af82210e3bc5aa84d502358d1
Akshay Deshmukh <deshmukhaksh@outlook.com>
2021-11-16 09:18:55 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Header from './Header'; 
 import PostFeed from './PostFeed';
-
-
+import CommentWidgets from "./CommentWidgets";
+import UploadPost from './UploadPost.js'
 
 function App() {
   return (
     <div>
     
       <Header />
+      <UploadPost />
       <PostFeed />
+      <CommentWidgets />
+      {/* <CommentBox /> */}
     </div>
   )
 }
