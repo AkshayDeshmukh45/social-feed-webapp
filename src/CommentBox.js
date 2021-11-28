0ffcf057287fffad19ceb95fbf2ac60bbcf0719c
Akshay Deshmukh <deshmukhaksh@outlook.com>
2021-11-28 11:32:42 +0530

    commit

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -7,34 +7,39 @@ function CommentBox() {
   const { TextArea } = Input;
   const [comment, setComment] = useState("");
 
-const submitCommit =(event)=>{
-event.preventDefault();
-}
+  function submitCommit(event) {
+    console.log(comment);
+    event.preventDefault();
+  }
 
   const onChange = (e) => {
     setComment(e.target.value);
   };
+  console.log(comment);
   return (
     <div className="commentbox">
-    
-    <Avatar
-              className="commentAvatar"
-              src={Profile}
-              width={10}
-              height={10}
-              layout="fixed"
-              style={{ marginLeft: "20px" }}
-            />
-          
-      <Form.Item onSubmit={submitCommit}>
-        <TextArea rows={2} onKeypress={onChange} placeholder="Write a comment here"
-        style={{width: "25vw",height: "20px",marginLeft:"10px",boxShadow: "1px 5px 7px -7px rgba(0, 0, 0, 0.75)" }}/>
-      </Form.Item>
-      {/* <Form.Item>
-        <Button htmlType="submit" type="primary">
-          Add Comment
-        </Button>
-      </Form.Item> */}
+      <Avatar
+        className="commentAvatar"
+        src={Profile}
+        width={10}
+        height={10}
+        layout="fixed"
+        style={{ marginLeft: "20px" }}
+      />
+
+      <Form onSubmit={submitCommit}>
+        <TextArea
+          rows={2}
+          onChange={onChange}
+          placeholder="Write a comment here"
+          style={{
+            width: "25vw",
+            height: "20px",
+            marginLeft: "10px",
+            boxShadow: "1px 5px 7px -7px rgba(0, 0, 0, 0.75)",
+          }}
+        />
+      </Form>
     </div>
   );
 }
